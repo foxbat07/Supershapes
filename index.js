3bf1e96eb15816495490c974059ae923a9366032
mohithingorani <hingoranimohit@gmail.com>
2020-04-26 17:06:59 -0700

    Update start port to 3000 for local

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const helmet = require('helmet')
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(compression({
