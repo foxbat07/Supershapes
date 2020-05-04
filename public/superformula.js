745d920ce63a5011e3410892e994a26042a224e1
mohithingorani <hingoranimohit@gmail.com>
2020-05-04 03:37:07 -0700

    Add line and points objects, tbd dropdown

--- public/superformula.js
+++ public/superformula.js
@@ -2,9 +2,12 @@
 // essentials
 var gui = new dat.GUI();
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer({ antialias: true });
-// var spriteMap = new THREE.TextureLoader().load("assets/circle-64.png" );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 100000 );
+var renderer = new THREE.WebGLRenderer({ 
+    antialias: true,
+    preserveDrawingBuffer: true
+});
+var spriteMap = new THREE.TextureLoader().load("assets/circle-64.png" );
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -21,10 +24,10 @@ lights[1] = new THREE.PointLight( 0xffffff, 1, 0 );
 lights[2] = new THREE.PointLight( 0xffffff, 1, 0 );
 lights[3] = new THREE.PointLight( 0xffffff, 1, 0 );
 
-lights[0].position.set( 0, 0, 200 );
-lights[1].position.set( 100, 200, 100 );
-lights[2].position.set( -100, -100, -100 );
-lights[3].position.set( 0, -100, 100 );
+lights[0].position.set( 0, 0, 2000 );
+lights[1].position.set( 1000, 2000, 1000 );
+lights[2].position.set( -1000, -1000, -1000 );
+lights[3].position.set( 0, -1000, 1000 );
 
 var controls = new function() {
     this.researchLink = function() {window.open('http://paulbourke.net/geometry/supershape/')};
@@ -38,22 +41,22 @@ var controls = new function() {
     this.heightSegments = 60;
     this.wireframe = false;
 
-    this.radius = 1;
-    this.a1 = 1;
-    this.b1 = 1;
+    this.radius = 200;
     this.m1 = 5;
     this.n11 = 1;
     this.n21 = 1;
     this.n31 = 2;
-
-    this.height = 1;
-    this.a2 = 1;
-    this.b2 = 1;
+    
+    this.height = 200;
     this.m2 = 5;
     this.n12 = 1;
     this.n22 = 1;
     this.n32 = 3;
     
+    this.a1 = 1;
+    this.b1 = 1;
+    this.a2 = 1;
+    this.b2 = 1;
 };
 
 var general = gui.addFolder('Superformula 3D | Mohit Hingorani');
@@ -67,43 +70,50 @@ general.addColor(controls, 'emissiveColor').name('Emissive Color');
 general.open();
 
 var f1 = gui.addFolder('Supershape 1');
-f1.add(controls, 'radius',1,5);
 f1.add(controls, 'm1',0,100).step(1).name('m1 - lat freq');
+f1.add(controls, 'radius',1,1000);
 f1.add(controls, 'n11',0.1,10);
 f1.add(controls, 'n21',0.1,10);
 f1.add(controls, 'n31',0.1,10);
-f1.open();
+f1.close();
 
 var f2 = gui.addFolder('Supershape 2');
 f2.add(controls, 'm2',0,100).step(1).name('m2 - long freq');
-f2.add(controls, 'height',1,5);
+f2.add(controls, 'height',1,1000);
 f2.add(controls, 'n12',0.1,10);
 f2.add(controls, 'n22',0.1,10);
 f2.add(controls, 'n32',0.1,10);
-f2.open();
+f2.close();
 
 var f3 = gui.addFolder('Extra Controls Supershape');
-f3.add(controls, 'a1',0.1,5);
-f3.add(controls, 'b1',0.1,5);
-f3.add(controls, 'a2',0.1,10);
-f3.add(controls, 'b2',0.1,10);
+f3.add(controls, 'a1',0.1,10);
+f3.add(controls, 'b1',0.1,10);
+f3.add(controls, 'a2',0.1,20);
+f3.add(controls, 'b2',0.1,20);
 f3.close();
 
 
 geometry = new THREE.CylinderGeometry( 1, 1, controls.height, controls.radialSegments, controls.heightSegments, true );
 var meshMaterial = new THREE.MeshPhongMaterial( { emissive: 0x000000, side: THREE.DoubleSide, flatShading: true, vertexColors: THREE.FaceColors } );
+var pointMaterial = new THREE.PointsMaterial( { blending: THREE.AdditiveBlending, color: 0xffffff, map: spriteMap,transparent: true, size: 0.5 } );
+var lineMaterial = new THREE.LineBasicMaterial( { color: 0xffffff } );
 // var meshMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors, side: THREE.DoubleSide } );			
 var superShape = new THREE.Mesh( geometry, meshMaterial );
+var superPoints = new THREE.Points( geometry, pointMaterial );
+var superLine = new THREE.Line( geometry, lineMaterial );
 
 init();
 animate();
 
 function init(){
-    camera.position.z = 3;			
+    camera.position.z = 500;			
     trackBallControls.rotateSpeed = 5;
     trackBallControls.zoomSpeed = 2;
 
-    scene.add(superShape);
+    // scene.add(superShape);
+    scene.add(superPoints);
+    // scene.add(superLine);
+
     scene.add(lights[0]);
     scene.add(lights[1]);
     scene.add(lights[2]);
@@ -111,18 +121,21 @@ function init(){
 }
 
 function animate() {
-    // superShape.rotation.x += 0.005;
     requestAnimationFrame(animate);
     trackBallControls.update();
+
+    scene.background = new THREE.Color(controls.backgroundColor);
+    meshMaterial.wireframe = controls.wireframe;
+    meshMaterial.emissive.set(new THREE.Color(controls.emissiveColor));
+    lineMaterial.color.set(new THREE.Color(controls.emissiveColor));
+    pointMaterial.color.setHex(controls.emissiveColor);
+
+    superPoints.geometry.colorsNeedUpdate = true;
+
     render();
 };
 
 function render() {
-    
-    scene.background = new THREE.Color(controls.backgroundColor);
-    meshMaterial.wireframe = controls.wireframe;
-    meshMaterial.emissive.set(new THREE.Color(controls.emissiveColor));
-
     // reset variables
     points = [];
     colors = [];
@@ -178,25 +191,18 @@ function render() {
         }
     }
     geometry.vertices = points;		// very important
-    
-    // for ( var i = 0; i < geometry.faces.length - 2; i += 2 ) {
-    // 	// var faceColor = Math.random() * 0xffffff;
-    // 	geometry.faces[i].color.setHex(0xffffff);
-    // 	geometry.faces[i+1].color.setHex(0x000000);
-    // }
         
-        for ( var i = 0; i < geometry.faces.length; i +=2 ) {
-            // var faceColor = Math.random() * 0xffffff;
-            // if(parseInt(i/120)%2 == 0) { alternate along long axis
-            if(i % (4 * parseInt(controls.stripes)) == 0) {
-                geometry.faces[i].color.setHex(controls.shapeColor1);
-                geometry.faces[i+1].color.setHex(controls.shapeColor1);
-            }
-            else {
-                geometry.faces[i].color.setHex(controls.shapeColor2);
-                geometry.faces[i+1].color.setHex(controls.shapeColor2);
-            }
+    for ( var i = 0; i < geometry.faces.length; i +=2 ) {
+        // if(parseInt(i/120)%2 == 0) { alternate along long axis
+        if(i % (4 * parseInt(controls.stripes)) == 0) {
+            geometry.faces[i].color.set(controls.shapeColor1);
+            geometry.faces[i+1].color.set(controls.shapeColor1);
         }
+        else {
+            geometry.faces[i].color.set(controls.shapeColor2);
+            geometry.faces[i+1].color.set(controls.shapeColor2);
+        }
+    }
             
     geometry.verticesNeedUpdate = true;
     geometry.elementsNeedUpdate = true;
